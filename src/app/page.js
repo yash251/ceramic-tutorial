143e384b5f8128770827dc01f6f6107c0a91a758
Yash Jagtap <jagtapyash2512@gmail.com>
2023-07-19 23:43:30 +0530

    connectToSelfID

--- src/app/page.js
+++ src/app/page.js
@@ -27,6 +27,11 @@ export default function Home() {
     return wrappedProvider;
   };
 
+  const connectToSelfID = async () => {
+    const ethereumAuthProvider = await getEthereumAuthProvider();
+    connect(ethereumAuthProvider);
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
