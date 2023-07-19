b7dfe0504cbfd61272c410c79454fc5bf0ea83e7
Yash Jagtap <jagtapyash2512@gmail.com>
2023-07-19 23:43:52 +0530

    getEthereumAuthProvider

--- src/app/page.js
+++ src/app/page.js
@@ -32,6 +32,13 @@ export default function Home() {
     connect(ethereumAuthProvider);
   };
 
+  const getEthereumAuthProvider = async () => {
+    const wrappedProvider = await getProvider();
+    const signer = wrappedProvider.getSigner();
+    const address = await signer.getAddress();
+    return new EthereumAuthProvider(wrappedProvider.provider, address);
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
