6bbd22b9eeaf6c450f8f34a92c45f73ca5c128fc
Yash Jagtap <jagtapyash2512@gmail.com>
2023-07-23 23:07:03 +0530

    updateRecordName

--- src/app/RecordSetter.js
+++ src/app/RecordSetter.js
@@ -1,5 +1,11 @@
 import { useViewerRecord } from "@self.id/react";
 
 function RecordSetter() {
+    const record = useViewerRecord("basicProfile");
 
+    const updateRecordName = async (name) => {
+        await record.merge({
+            name: name,
+        });
+    };
 }
